cf8cd46aac00a33f10ac876dc66a007d53fdac31
Reena <reena.a@msds.christuniversity.in>
2023-02-15 16:10:16 +0530

    Lab3

--- LAB 3/README.md
+++ LAB 3/README.md
@@ -1,58 +1,133 @@
-OUTPUT
-Enter exit when cued to enter values to stop entering values in the array.
+**QUESTION**
+Create a java program that performs the following operation:  
+The program should collect an integer array from the user  
+After the array is entered, you need to create a menu of items 
+Find the Mean Value  
+Find the Median Value  
+Find the Mode Value   
 
-Enter a value: 1
+From the user-given array.  
+The program should have a minimum of 3 functions apart from the main function; each of the functions implements the Mean, Median, and Mode computation by accepting the array user has entered as a parameter and returning the value as a result. From the main method, you will display the result.
 
-Enter a value: 2
+**PROGRAM PSEUDO CODE** 
+The contains four main functions: 
 
-Enter a value: 3
+*create a global variable 'length' that store the total number of enter elements 
 
-Enter a value: 3
+*FUNCTION readArray()*: 
+    * create an integer array of a large size
+    * create an infinte loop
+        *read user input
+        * check if the input is equal to 'exit'. 
+            If it is:  
+                -set 'length' as the total number of times you executed the infinte loop  
+                -exit the infinite loop   
+            otherwise:  
+                -continue  
+        *check if the input is an integer. 
+            If it is:  
+                -add value to your array
+            otherwise:  
+                -print an appropriate error message.  
 
-Enter a value: 5
+*FUNCTION Mean(int array[])*:
+    *iterate through the non-null elements of the array recieved through the parameters
+    *add the iterating values
+    *return the total sum divided by 'length'
 
-Enter a value: 4
+*FUNCTION Median(int array[])*:
+    *sort the array recieved through the parameters
+    *return the middle value of the array ('length'/2)  
 
-Enter a value: exit
+*FUNCTION Mode(int array[])*:
+    *create an array countArray[] with size equal to 'length'
+    *iterate through the value of the array recieved through the parameters and count how many times that value occurs in the array. Store the count in countArray.
+    *find the maximum count value in countArray
+    *return the value in 'array' corresponding to the maximum count value obtained.  
+
+**FLOW CHART**
+
+**SAMPLE OUTPUT**  
+
+Enter exit when cued to enter values to stop entering values in the array.  
+
+Enter a value: 1    
+
+Enter a value: 2  
+
+Enter a value: 2  
+
+Enter a value: 3  
+
+Enter a value: 0  
+
+Enter a value: 7  
+
+Enter a value: r  
+----------------------------  
+     Enter only numbers  
+----------------------------  
 
 
-ENTER EXIT TO STOP EXECUTING THIS PROGRAM
+Enter a value: 5  
 
-Enter
- 1 to find the mean.
- 2 to find the median.
- 3 to find the mode.
-3
+Enter a value: 7  
 
-Mode: 3
-Enter
- 1 to find the mean.
- 2 to find the median.
- 3 to find the mode.
-1
-Mean: 3.0
-Enter
- 1 to find the mean.
- 2 to find the median.
- 3 to find the mode.
-2
+Enter a value: exit  
 
-Median: 3
-Enter
- 1 to find the mean.
- 2 to find the median.
- 3 to find the mode.
-4
-Invlaid Input
-Enter
- 1 to find the mean.
- 2 to find the median.
- 3 to find the mode.
-2
 
-Median: 3
-Enter
- 1 to find the mean.
- 2 to find the median.
- 3 to find the mode.
-exit
+ENTER EXIT TO STOP EXECUTING THIS PROGRAM  
+
+Enter  
+ 1 to find the mean.  
+ 2 to find the median.  
+ 3 to find the mode.  
+1  
+----------------------------  
+         Mean: 3.38  
+----------------------------  
+
+Enter  
+ 1 to find the mean.  
+ 2 to find the median.  
+ 3 to find the mode.  
+2  
+----------------------------  
+         Median: 2  
+----------------------------  
+
+Enter    
+ 1 to find the mean.    
+ 2 to find the median.    
+ 3 to find the mode.    
+3      
+----------------------------    
+         Mode: 2    
+----------------------------    
+
+Enter  
+ 1 to find the mean.  
+ 2 to find the median.  
+ 3 to find the mode.  
+4  
+----------------------------  
+        Invalid Input!  
+----------------------------   
+
+Enter  
+ 1 to find the mean.  
+ 2 to find the median.  
+ 3 to find the mode.  
+reena  
+----------------------------  
+     Enter only numbers  
+----------------------------  
+
+Enter    
+ 1 to find the mean.   
+ 2 to find the median.    
+ 3 to find the mode.    
+ exit    
+----------------------------    
+            BYE!    
+----------------------------    
--- LAB 3/lab3.java
+++ LAB 3/lab3.java
@@ -1,83 +1,121 @@
+//package for input output functions
 import java.util.*;
-import java.io.*;
+//package to print only two decimal points
+import java.text.DecimalFormat;
 
 public class lab3 {
+    /*creating a single variable that can be accessible to all fucntions that stores the number of non-zero
+    elements in the array*/
     static int length = 0;
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED_BACKGROUND= "\u001B[33m";
+
+    //creating variables to beautify the output
+    static String black = "\u001B[0m";
+    static String red ="\u001b[31m";
+    static String blue="\u001b[34m";
+    static String green="\u001b[32m";
+
+    //main function
     public static void main(String args[]) {
+        //intializing array ans assigning the values entered by the user by calling the function readArray()
         int[] arr = readArray();
         
         Scanner sc = new Scanner(System.in);
-        System.out.print("\n\nENTER EXIT TO STOP EXECUTING THIS PROGRAM\n\n");
+        //letting the user know to type exit to leave the loop
+        System.out.print(green+"\n\nENTER EXIT TO STOP EXECUTING THIS PROGRAM\n\n"+black);
+
+        //creating an infite loop to continuously allow the user to redo the any of the actions listed below
         while (true) {
             System.out.println("Enter\n 1 to find the mean.\n 2 to find the median.\n 3 to find the mode.");
+
+            //reading user's choice
             String choice = sc.nextLine();
+
+            //checking if the user wants to stop execution of the program
             if (choice.equalsIgnoreCase("exit")) {
+                System.out.print(green+"----------------------------\n            BYE!\n----------------------------\n"+black+"\n");
                 break;
             } else {
                 try {
+                    //converting the string to integer
                     int c = Integer.parseInt(choice);
                     switch (c) {
                         case 1:
+                            //call the mean function to generate the mean of the array
                             double mean = Mean(arr);
-                            System.out.println("\nMean: " + mean+"\n");
+                            DecimalFormat df = new DecimalFormat("#.##");
+                            System.out.println(blue+"----------------------------\n         Mean: " + df.format(mean)+"\n----------------------------\n"+black);
                             break;
 
                         case 2:
+                            //call the Median function to generate the median of the array
                             int median = Median(arr);
-                            System.out.println("\nMedian: " + median+"\n");
+                            System.out.println(blue+"----------------------------\n         Median: " + median+"\n----------------------------\n"+black);
                             break;
 
                         case 3:
+                            //call the Mode function to generate the mode of the array
                             int mode = Mode(arr);
-                            System.out.println("\nMode: " + mode+"\n");
+                            System.out.println(blue+"----------------------------\n         Mode: " + mode+"\n----------------------------\n"+black);
                             break;
 
                         default:
-                            System.out.println("\n"+ANSI_RED_BACKGROUND+"----------------------------\nInvalid Input!\n----------------------------\n"+ANSI_RESET+"\n\n");
+                            //display an error message for entering an invalid number
+                            System.out.println("\n"+red+"----------------------------\n        Invalid Input!\n----------------------------\n"+black+"\n\n");
                     }
                 } catch (NumberFormatException e) {
-                    System.out.print("\n"+ANSI_RED_BACKGROUND+"----------------------------\nEnter only numbers\n----------------------------\n"+ANSI_RESET+"\n\n");
+                    /**if the conversion from string to int throws an error, it implies that the entered value was not an
+                    integer. Therefore, we used try catch to catch these errors and display an appropriate message **/
+                    System.out.print(red+"----------------------------\n     Enter only numbers\n----------------------------\n"+black+"\n");
                 }
             }
 
         }
     }
 
+    //function to read elements of an array
     public static int[] readArray() {
         int[] arr = new int[1024];
         int c = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter exit when cued to enter values to stop entering values in the array.");
+        System.out.println(green+"\nEnter exit when cued to enter values to stop entering values in the array."+black);
         for (int i = 0;; i++) {
             System.out.print("\nEnter a value: ");
             String a = sc.nextLine();
+            //checking if the user wants to exit or stop entering values
             if (a.equalsIgnoreCase("exit")) {
                 length = i;
                 break;
             } else {
                 try {
+                    /*converting the user input to interger. If this conversion throws and error, it implies the user entered
+                     * a value that is not an integer. To handle the situation we use the try catch.*/
                     c = Integer.parseInt(a);
                     arr[i] = c;
                 } catch (NumberFormatException e) {
-                    System.out.println("Enter only numbers");
+                    /**displaying an appropriate error message when the user does not enter an integer */
+                    System.out.print(red+"----------------------------\n     Enter only numbers\n----------------------------\n"+black+"\n");
+                    i-=1;
                 }
             }
         }
 
+        //returning the newly created array
         return arr;
     }
 
+    //function that generates the mean of the array passed as parrameters
     public static double Mean(int arr[]) {
         double avg = 0.0;
         for (int i = 0; i < length; i++) {
             avg += arr[i];
         }
+        //returning the average of the array as a double value
         return (avg / length);
     }
 
+    //function that generates the median
     public static int Median(int arr[]) {
+        //sorting the array recieved through parameters
         for (int i = 0; i < length; i++) {
             for (int j = i + 1; j < length; j++) {
                 if (arr[i] < arr[j]) {
@@ -87,10 +125,17 @@ public class lab3 {
                 }
             }
         }
+
+        /*The middle element of the sorted array is the median. Therefore, we sorted the array
+         * and return the middle value*/
         return (arr[(int) (length / 2)]);
     }
 
+
+    //function that generates the last most occuring element
     public static int Mode(int arr[]) {
+        /*creating an array the size of the total number of non-zero elements and storing the 
+         * count of each element based corresponding to their indexes*/
         int[] countArray = new int[length];
         int count = 0;
         for (int i = 0; i < length; i++) {
@@ -103,6 +148,7 @@ public class lab3 {
             count = 0;
         }
 
+        //finding the max count value from countArray
         for (int a = 0; a < length; a++) {
             for (int b = a + 1; b < length; b++) {
                 if (countArray[a] < countArray[b]) {
@@ -110,7 +156,8 @@ public class lab3 {
                 }
             }
         }
-
+        //returning the value in the array received through the parameters based on the index of the maximum
+        //value of count
         return arr[count];
     }
 }
