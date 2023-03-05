2c9f79acb332a168e59c8fd216dcac0fa4d126d6
Reena <reena.a@msds.christuniversity.in>
2023-03-05 08:42:24 +0530

    LAB4

---
+++ LAB 4/lab4.java
@@ -0,0 +1,23 @@
+class lab4{
+
+    public static void main(String args[]){
+    
+    }
+
+    lab4(String name,String accountNumber, double bankBalance){
+
+    }
+
+    public void withdraw(){
+
+    }
+
+    public void printTransaction(){
+
+    }
+
+    public void printAccoutnSummary(){
+        
+    }
+    
+}
